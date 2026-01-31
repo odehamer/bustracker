40763db6cb2bcc7ebf1907e22d41f97df82288c3
odehamer <oscar@dehamer.com>
2026-01-30 19:51:04 -0800

    small changes

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ options.chain_length = 1
 options.parallel = 1
 options.hardware_mapping = 'regular'
 options.gpio_slowdown = 2
+options.disable_hardware_pulse = True
 matrix = RGBMatrix(options=options)
 
 def fetch_bus_data():
